112ac30ab0a2d9390dac20e395ec76cd2ce9b646
jimfox27 <jim.cabrera@alumnos.duoc.cl>
2020-10-05 09:33:27 -0300

    se agrego la opcion dos al menú

--- menu.py
+++ menu.py
@@ -8,10 +8,29 @@ def Temperaturas():
         tempe=float(input("Ingrese temperatura: "))
         suma=suma+tempe
     print("El promedio de las temperaturas es: ", round((suma/veces),2))
-    tecla=input("presione una tecla para continuar")
+    tecla=input("Presione una tecla para continuar")
 
 def personas():
     print("--- Opción de Datos de Personas ---")
+    #ingresar para n personas el nombre y la edad. n debe ser ingresado por teclado
+    #mostrar: cuantas personas son mayores de edad y cuantas son menores de edad.
+    #subir un tercer commit a git con el mensaje "se agrego la opcion dos al menú"
+    
+    mayores = 0
+    menores = 0
+    x = 1
+    n=int (input("Cuantas personas desea ingresar?: "))
+    while (x<=n):        
+        input("Nombre de la persona?: ")
+        edad=int (input("Que Edad tiene?: "))
+        if(edad <= 18):
+            mayores = mayores + 1
+        else:
+            menores = menores + 1
+        x = x + 1
+    print("Cantidad de Mayores de edad: " + str(mayores))
+    print("Cantidad de Manosres de edad: " + str(menores))
+    tecla=input("Presione una tecla para continuar")
 
 seguir = True
 while seguir:
